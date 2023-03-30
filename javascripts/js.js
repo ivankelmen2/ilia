2bae6523de5779aefd4252e2ae8b3b8e7aa90c8d
ivankelmen <ivankelmen@gmail.com>
2023-03-30 17:08:12 +0300

    summ
    sdf

---
+++ javascripts/js.js
@@ -0,0 +1,5 @@
+<script src="javascripts/jquery-3.6.3.js" charset="utf-8"></script>
+$( function() {
+    $(".icon1").draggable();
+  })
+<script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js">;
